27b9e36d5b6462e93f86a303a05b8e97c88c668d
Erenay Karacan <erenay@selfdecode.com>
2023-01-04 01:17:20 +0100

    start the eval page

--- HAE_demonstrator/urls.py
+++ HAE_demonstrator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from .views import home_page, train_page, train_page_qvc
+from .views import home_page, train_page, train_page_qvc, evaluate_hae,evaluate_qvc, visualize, evaluate_metric
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +26,10 @@ urlpatterns = [
     path("", home_page),
     path("train/hae/", train_page),
     path("train/qvc/", train_page_qvc),
+    path("evaluate/hae/", evaluate_hae),
+    path("evaluate/qvc/", evaluate_qvc),
+    path("evaluate/metric/", evaluate_metric),
+    path("visualize/", visualize),
     path("train/", include('train.urls')),
 ] 
 
--- HAE_demonstrator/views.py
+++ HAE_demonstrator/views.py
@@ -1,6 +1,16 @@
+import os
+dirname = os.path.dirname(__file__)
+
 from django.shortcuts import render
+from django.http import HttpResponse
 from .constants import MW_measure, SIM_expr, N_PARAMS
 import json
+
+import sys
+sys.path.append(os.path.join(dirname, '../../HAE/modules/'))
+from preprocessing.preprocessing import sample_test_data
+from metrics import metrics, predict
+from preprocessing.visualize import plot_PCA_2D
 
 
 def home_page(request):
@@ -31,3 +41,73 @@ def train_page_qvc(request):
 
 
     return render(request, "train_qvc.html", dic)
+
+
+def evaluate_hae(request):
+    dic = {
+        "current_page": "evaluate",
+    }
+    dic = dic | MW_measure
+    dic = dic | SIM_expr
+    
+    files = {}
+    for i in range(len(N_PARAMS.keys())):
+        try:
+            files["FILES" + str(i+1)] = json.dumps(os.listdir(os.path.join(dirname, "../../HAE/data/training_results/pqc" + str(i+1) + "/binary_cl/")))
+        except FileNotFoundError:
+            files["FILES" + str(i+1)] = []
+    dic = dic | files
+
+    return render(request, "evaluate_hae.html", dic)
+
+
+def evaluate_qvc(request):
+    dic = {
+        "current_page": "evaluate",
+    }
+    dic = dic | MW_measure
+    dic = dic | SIM_expr
+    
+    binary_files = {}
+    multi_files = {}
+    for i in range(len(N_PARAMS.keys())):
+        try:
+            binary_files["BINARY_FILES" + str(i+1)] = json.dumps(os.listdir(os.path.join(dirname, "../../HAE/data/training_results_QVC/pqc" + str(i+1) + "/binary_cl/")))
+        except FileNotFoundError:
+            binary_files["BINARY_FILES" + str(i+1)] = []
+
+        try:
+            multi_files["MULTI_FILES" + str(i+1)] = json.dumps(os.listdir(os.path.join(dirname, "../../HAE/data/training_results_QVC/pqc" + str(i+1) + "/multi_cl/")))
+        except FileNotFoundError:
+            binary_files["MULTI_FILES" + str(i+1)] = []
+    dic = dic | binary_files
+    dic = dic | multi_files
+
+    return render(request, "evaluate_qvc.html", dic)
+
+
+def evaluate_metric(request):
+    n_samples = int(request.GET["n_samples"])
+    test_data, test_labels = sample_test_data(n_samples, True)
+    model = request.GET["model"]
+
+    # TODO: call this from celery with predictions
+    fig = plot_PCA_2D(test_data=test_data, test_labels=test_labels, path_save=os.path.join(dirname, f"../static/eval/scatter/{model}_{n_samples}.png"))
+
+
+    if model == "HAE":
+        return HttpResponse("A")
+    elif model == "QVC":
+        return HttpResponse("A")
+    else:
+        raise KeyError("Given Model can be either HAE or QVC")
+
+
+    return HttpResponse("A")
+
+
+def visualize(request):
+    dic = {
+        "current_page": "visualize",
+    }
+    return render(request, "visualize.html", dic)
