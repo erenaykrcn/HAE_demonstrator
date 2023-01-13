6dd88ba9f48a20246e52c450c334f42a34f42c60
Erenay Karacan <erenay@selfdecode.com>
2023-01-13 16:39:00 +0100

    add the binary classical classifier

--- evaluate/tasks.py
+++ evaluate/tasks.py
@@ -9,7 +9,7 @@ from train.models import ActiveTask
 import sys
 sys.path.append(os.path.join(dirname, '../../HAE/modules/'))
 from metrics.metrics import get_scores
-from metrics.predict import predict_HAE, predict_classical, predict_QVC
+from metrics.predict import predict_HAE, predict_classical_binary_cl, predict_QVC
 from preprocessing.visualize import plot_PCA_2D
 
 
@@ -25,7 +25,13 @@ def test_model_task(self, job, test_data, test_labels, path_save="", custom_dict
 	pqc = int(job["pqc"]) if job["pqc"] else 0
 	n_samples = int(job["n_samples"])
 	test_job = TestJob.objects.get(id=job["id"])
-	predict_cl, labels_cl = predict_classical(test_data=test_data, test_labels=test_labels)
+	predict_cl, labels_cl = predict_classical_binary_cl(test_data=test_data, test_labels=test_labels)
+	f1_cl, precision_cl, recall_cl = get_scores(predict_cl, labels_cl)
+
+	test_job.precision_cl = precision_cl
+	test_job.recall_cl = recall_cl
+	test_job.f1_cl = f1_cl
+	test_job.save()
 
 	if model == "HAE":
 		predict, labels = predict_HAE(qc_index=pqc, path=test_job.result_path, test_data=test_data, test_labels=test_labels, custom_dict=custom_dict)
@@ -33,15 +39,10 @@ def test_model_task(self, job, test_data, test_labels, path_save="", custom_dict
 		predict, labels = predict_QVC(qc_index=pqc, path=test_job.result_path, test_data=test_data, test_labels=test_labels, custom_dict=custom_dict)
 
 	f1, precision, recall = get_scores(predict, labels)
-	f1_cl, precision_cl, recall_cl = get_scores(predict_cl, labels_cl)
 
 	test_job.precision = precision
 	test_job.recall = recall
 	test_job.f1 = f1
-
-	test_job.precision_cl = precision_cl
-	test_job.recall_cl = recall_cl
-	test_job.f1_cl = f1_cl
 
 	test_job.status = "completed"
 	test_job.save()
